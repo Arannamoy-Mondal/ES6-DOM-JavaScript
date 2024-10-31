2f9a1d02d7611920d9de9f89a329d15b158b443d
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-10-31 18:37:37 +0600

    Add 4_API_JSON_Holder.js

---
+++ DOM/4_API_JSON_Holder.js
@@ -0,0 +1,11 @@
+//Copy paste on the browser console and hit enter and enjoy the show
+//For more information search on google
+
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(function(res){
+//         return res.json()
+//       })
+//       .then(function(json){
+//         console.log(json)
+//       })
