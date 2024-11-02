a130a07aa80b6c7dd1b7a018efb26e1a98351624
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-11-02 12:07:24 +0600

    add onclick()

---
+++ DOM/25_onclick_function.js
@@ -0,0 +1,11 @@
+let darkMode=document.getElementById("darkMode")
+darkMode.style.padding="10px"
+darkMode.style.fontSize="1rem"
+darkMode.style.background="#222"
+darkMode.style.color="white"
+darkMode.style.marginLeft="40%"
+darkMode.style.marginTop="50px"
+darkMode.style.marginBlock="150px"
+const bgBlue=()=>{
+    document.getElementById("body").style.background="#222"
+}
